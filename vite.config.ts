18be145610bb291bff0cfaf9c71ce03cbbb6c7fb
Bruno Braga <bf.braga@campus.fct.unl.pt>
2025-04-26 11:56:18 +0100

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vite.dev/config/
+/** @type {import('vite').UserConfig} */
+dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${process.env.REPONAME}/` || '/portfolio/'
 })
